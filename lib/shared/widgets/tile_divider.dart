6d84c4f236d2dfc0f6edfa8976ba217e18facf2c
Matin <matin7najafi@gmail.com>
2025-12-24 01:37:16 +0330

    Add a custom thickness to divider widget

--- lib/shared/widgets/tile_divider.dart
+++ lib/shared/widgets/tile_divider.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:mazeh_app/core/constants/app_color.dart';
 
 class TileDivider extends StatelessWidget {
-  const TileDivider({super.key, this.endIndent = 0});
+  const TileDivider({super.key, this.endIndent = 0, this.thickness = 0.5});
   final double endIndent;
+  final double thickness;
 
   @override
   Widget build(BuildContext context) {
     return Divider(
-      thickness: 0.5,
+      thickness: thickness,
       color: AppColor.backgroundColor,
       endIndent: endIndent,
     );
